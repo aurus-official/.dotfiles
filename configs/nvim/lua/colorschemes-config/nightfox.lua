aa6ecc1ce099efb26b39eb4461abf44b02c48592
Russel Pardillo Quinto <113511457+aurus-official@users.noreply.github.com>
2024-02-24 17:44:35 +0800

    Delete configs directory

--- configs/nvim/lua/colorschemes-config/nightfox.lua
+++
@@ -1,7 +0,0 @@
-function Colors(color)
-    color = color or "carbonfox"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-end 
